fd6f35e8503e4071675551fae6a31d0578985c89
mycoolfin <michaelfinn@live.com.au>
2018-08-08 17:06:17 +0800

    Upgraded Camera script

--- tracker/camera.py
+++ tracker/camera.py
@@ -1,4 +1,5 @@
 import cv2
+from threading import Thread
 import time
 
 
@@ -6,13 +7,39 @@ CAPTURE_WIDTH = 640
 CAPTURE_HEIGHT = 480
 
 class Camera:
-    def __init__(self):
-        self.stream = cv2.VideoCapture(0)
-        self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, CAPTURE_WIDTH)
-        self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, CAPTURE_HEIGHT)
-    def get_frame(self):
-        _, frame = self.stream.read()
-        return frame
-    def destroy(self):
-        self.stream.release()
-        cv2.destroyAllWindows()
+	def __init__(self):
+		self.stream = cv2.VideoCapture(0)
+		self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, CAPTURE_WIDTH)
+		self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, CAPTURE_HEIGHT)
+
+		_, self.frame = self.stream.read()
+		self.stopped = False
+
+		self.start()
+
+	def start(self):
+		Thread(target=self.update, args=()).start()
+		return self
+
+	def update(self):
+		while True:
+			if self.stopped:
+				return
+			_, self.frame = self.stream.read()
+
+	def get_frame(self):
+		return self.frame
+
+	def stop(self):
+		self.stopped = True
+
+	def destroy(self):
+		self.stop()
+		self.stream.release()
+
+
+if __name__ == "__main__":
+	cam = Camera()
+	while True:
+		cv2.imshow("", cam.get_frame())
+		cv2.waitKey(1)
